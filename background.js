c2de8de367d4205b32887b8b9f5766e73484ec4e
Vadim Bida <vadim.prog.2000@gmail.com>
2023-03-13 17:20:37 +1000

    clear code

--- background.js
+++ background.js
@@ -1,26 +1,14 @@
 chrome.action.disable()
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	console.log(1)
 	if (changeInfo.status === "loading") {
-		console.log(2)
-		console.log(tab.url)
-		console.log(
-			tab.url.includes("https://steamcommunity.com/market/listings/730")
-		)
 		if (tab.url.includes("https://steamcommunity.com/market/listings/730")) {
-			console.log(3)
-			chrome.scripting.insertCSS(
-				{
-					files: ["./style.css"],
-					target: {
-						tabId,
-					},
+			chrome.scripting.insertCSS({
+				files: ["./style.css"],
+				target: {
+					tabId,
 				},
-				() => {
-					console.log("Inserted")
-				}
-			)
+			})
 		}
 	}
 	if (changeInfo.status === "complete") {
